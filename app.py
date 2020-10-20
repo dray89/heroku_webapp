8b8a3c70a35b1b4682afe67f2f01278a05ce56f1
Debra Ray <raydebra89@gmail.com>
2020-10-20 18:49:04 -0500

    initial deploy

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 from flask import Flask, render_template, request
+from flask-sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+#ENV = 'dev'
+ENV = 'prod'
+
+if ENV== 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/databasename'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://dpnsjvnfohxnbe:0d696bcfe4f2ddbb7c668e81176495b4f0204aafbaf6e3483d7da373082e80e1@ec2-23-22-156-110.compute-1.amazonaws.com:5432/d4pb7s5vdspj5m'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class database(db.Model):
+    __tablename__ = 'data'
+    id = db.Column(db.Integer, primary_key=True)
+    column1 = db.Column(db.String(200), unique=True)
+    column2 = db.Column(db.String(200), unique=True)
+
+    def __init__(self, column1, column2):
+        self.column1 = column1
+        self.column2 = column2
 
 @app.route('/')
 def index():
@@ -7,7 +32,7 @@ def index():
 
 
 ''' 
-how to do post request
+how to do post request / Form
 
  @app.route(/submit, methods=['POST'])
  def submit():
@@ -15,9 +40,15 @@ how to do post request
          field1 = request.form[FIELD1]
          field2 = request.form[FIELD2]
          print(field1, field2)
-         if customer== '' or dealer =='':
+         if field1== '' or field2 =='':
              return render_template('index.html', message="please enter required fields")
-         return render_template(success.html))
+         if db.session.query(Feedback).filter(Feedback.field1 ==field1).count() ==0:
+            data = Feedback(field1, field2)
+            db.session.add(data)
+            db.session.commit()
+            return render_template(success.html))
+         return render_template('index.html', message="Already Submitted")
+
 '''
 
 if __name__ == "__main__":
